09676c9b9f94687f86e3ae806262dca624f2faa8
Beopard <93901813+Beopard@users.noreply.github.com>
2022-06-08 18:19:04 +0900

    test

--- app/src/main/java/com/example/anonymousboard/ui/DetailFragment.kt
+++ app/src/main/java/com/example/anonymousboard/ui/DetailFragment.kt
@@ -2,6 +2,7 @@ package com.example.anonymousboard.ui
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -24,6 +25,7 @@ class DetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        Log.d("test","test");
 
     }
 
