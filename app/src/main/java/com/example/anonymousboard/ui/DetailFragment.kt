d5529e54a5c05d82797b2fc170506135144f4383
Beopard <93901813+Beopard@users.noreply.github.com>
2022-06-08 18:15:34 +0900

    1

---
+++ app/src/main/java/com/example/anonymousboard/ui/DetailFragment.kt
@@ -0,0 +1,35 @@
+package com.example.anonymousboard.ui
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.anonymousboard.R
+import com.example.anonymousboard.databinding.DetailFragmentBinding
+import com.example.anonymousboard.databinding.MainFragmentBinding
+
+class DetailFragment : Fragment() {
+    private var _binding: DetailFragmentBinding?=null
+    private val binding get()= _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding= DetailFragmentBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
---
+++ app/src/main/java/com/example/anonymousboard/ui/EditFragment.kt
@@ -0,0 +1,36 @@
+package com.example.anonymousboard.ui
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.anonymousboard.R
+import com.example.anonymousboard.databinding.EditFragmentBinding
+import com.example.anonymousboard.databinding.MainFragmentBinding
+
+class EditFragment : Fragment() {
+
+    private var _binding: EditFragmentBinding?=null
+    private val binding get()= _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding= EditFragmentBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
